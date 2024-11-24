75ba324ebfb2fb7ffa6cb25c1568cf88cb6231ee
agarciafdz <agarciafdz@gmail.com>
2024-11-23 22:44:36 -0600

    adde more to my wishlist

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,5 @@
 
 print("Hello, World!")
 print("and this should add another line")
+print("What else do I want to do?")
+print("Create a github action? that can handle only centain directories and only certain languages a")
